9580a92b5d1b7a14c512d82e346e779763cc3fc0
Malte Rohde <malte.rohde@gmx.de>
2012-09-02 18:27:09 +0200

    Add activated animation to tracks in playlist.

--- app/assets/javascripts/playlist.js
+++ app/assets/javascripts/playlist.js
@@ -6,9 +6,22 @@ $(document).ready(function() {
 	});
 	
 	$("#playlist_scrollpane").jScrollPane();
-
 });
 
 function playlist_update() {
 	$("#playlist_scrollpane").data("jsp").reinitialise({"mouseWheelSpeed": 50});
+
+	$('.playlist_item_tracks_item').bind('webkitAnimationEnd', function() {
+		this.style.webkitAnimationName = '';
+	});
+
+	$('.playlist_item_tracks_item').bind('webkitAnimationEnd', function(){
+    this.style.webkitAnimationName = '';
+	});
+
+	$('.playlist_item_tracks_item').click(function(){
+		console.log($(this).data("trackid"));
+	  $(this).css('webkitAnimationName', 'playlist_item_tracks_activated');
+	});
+
 }
